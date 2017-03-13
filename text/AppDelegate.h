4ca20d5a30a5516417d71b737fcfeaeb7f6bd50b
qingyou <398487059@qq.com>
2017-03-13 17:41:56 +0800

    XX

---
+++ text/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  text
+//
+//  Created by 姚庆友 on 2017/3/13.
+//  Copyright © 2017年 姚庆友. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ text/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  text
+//
+//  Created by 姚庆友 on 2017/3/13.
+//  Copyright © 2017年 姚庆友. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
